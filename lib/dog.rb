88b1fec830a25150ae6f3e62cd1866fa0287f1ad
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 08:23:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog(name)
+  name=Fido
+  :@name= fido
+end
+fido= Dog.new
+fido.name=fido
+end
+
+class Dog(breed)
+  name= Beagle
+  :@name= Beagle
+end
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+end
+end
